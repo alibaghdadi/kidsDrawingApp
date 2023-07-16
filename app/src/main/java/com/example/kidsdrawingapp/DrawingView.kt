0452754956003eee10f34dd9f218ed2e5eb22667
Ali Baghdadi <alibaghdadi.developer@gmail.com>
2023-07-16 11:57:02 +0300

    add initialization values in DrawingView class

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
+import android.icu.text.TimeZoneFormat.ParseOption
 import android.util.AttributeSet
 import android.view.View
 
@@ -17,7 +18,22 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mCanvasPaint: Paint? = null
     private var canvas: Canvas? = null
     private var color = Color.BLACK
-    private var mBrushSize: Float = 0.toFloat()
+    private var mBrushSize: Float = 0F
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20F
+    }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
