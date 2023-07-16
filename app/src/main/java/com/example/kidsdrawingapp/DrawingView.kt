7adcdd13afbd677cbaa0a60f40cd3899741e64bb
Ali Baghdadi <alibaghdadi.developer@gmail.com>
2023-07-16 09:58:15 +0300

    adding DrawingView class properties

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -1,11 +1,23 @@
 package com.example.kidsdrawingapp
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var canvas: Canvas? = null
+    private var color = Color.BLACK
+    private var mBrushSize: Float = 0.toFloat()
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
