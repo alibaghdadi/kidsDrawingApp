f6a371e2652695330727f307e82f582f459ef9e4
Ali Baghdadi <alibaghdadi.developer@gmail.com>
2023-07-16 09:50:25 +0300

    adding CustomPath inner class

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -1,9 +1,13 @@
 package com.example.kidsdrawingapp
 
 import android.content.Context
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
 }
